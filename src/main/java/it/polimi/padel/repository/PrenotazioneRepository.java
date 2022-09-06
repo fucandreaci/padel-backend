b001206b4dff4072e4ffd362b2efb9946d4f927e
Andrea Fucci <afucci@soldo.com>
2022-09-06 18:31:09 +0200

    Implementata verifica di un campo libero

--- src/main/java/it/polimi/padel/repository/PrenotazioneRepository.java
+++ src/main/java/it/polimi/padel/repository/PrenotazioneRepository.java
@@ -7,8 +7,13 @@ package it.polimi.padel.repository;/*
  */
 
 import it.polimi.padel.model.Prenotazione;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
-public interface PrenotazioneRepository extends CrudRepository<Prenotazione, Integer> {
+import java.time.LocalDateTime;
+import java.util.List;
 
+public interface PrenotazioneRepository extends CrudRepository<Prenotazione, Integer> {
+    @Query("SELECT p FROM Prenotazione p WHERE p.campo.id = ?1 AND p.da < ?3 AND p.a > ?2")
+    public List<Prenotazione> isCampoLibero (Integer idCampo, LocalDateTime da, LocalDateTime a);
 }
--- src/main/java/it/polimi/padel/service/PrenotazioneService.java
+++ src/main/java/it/polimi/padel/service/PrenotazioneService.java
@@ -6,11 +6,20 @@ package it.polimi.padel.service;/*
  * Copyright © 2022-2022 Andrea Fucci
  */
 
+import it.polimi.padel.repository.PrenotazioneRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
 
 @Service
 @Transactional
 public class PrenotazioneService {
+    @Autowired
+    private PrenotazioneRepository prenotazioneRepository;
+
+    public boolean isCampoLibero (Integer idCampo, LocalDateTime da, LocalDateTime a) {
+        return !prenotazioneRepository.isCampoLibero(idCampo, da, a).isEmpty();
+    }
 }
