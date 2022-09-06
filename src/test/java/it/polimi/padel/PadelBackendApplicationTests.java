6b73c280121f107fd63549d8cea466b13bf82b9e
Andrea Fucci <afucci@soldo.com>
2022-09-06 13:15:25 +0200

    first commit

---
+++ src/test/java/it/polimi/padel/PadelBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package it.polimi.padel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PadelBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
